8a8cac6b7ea55b85c5c278f830e820272659afdb
faretek <107722825+FAReTek1@users.noreply.github.com>
2025-09-10 22:55:49 +0100

    feat: CLI

--- scratch_cli/__main__.py
+++ scratch_cli/__main__.py
@@ -1,11 +1,15 @@
 import argparse
 
+from scratch_cli.__about__ import __version__
+
 def main():
     parser = argparse.ArgumentParser(
         prog='scratch',
         description='Scratch command line interface',
-        epilog=f""
+        epilog=f"Scratch CLI {__version__}"
     )
+
+    args = parser.parse_args()
 
 if __name__ == '__main__':
     main()
