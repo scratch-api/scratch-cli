e4eced50d327f5297a7100b72b9df184ae1911c4
faretek <107722825+FAReTek1@users.noreply.github.com>
2025-09-10 19:15:50 +0100

    feat: add argparser

---
+++ scratch-cli/__main__.py
@@ -0,0 +1,11 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='scratch',
+        description='Scratch command line interface',
+        epilog=f""
+    )
+
+if __name__ == '__main__':
+    main()
