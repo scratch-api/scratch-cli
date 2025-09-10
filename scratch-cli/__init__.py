daa4d2d62f16ca60c94c0e625ffb6729973a9a96
faretek <107722825+FAReTek1@users.noreply.github.com>
2025-09-10 19:14:28 +0100

    feat: __init__

---
+++ scratch-cli/__init__.py
@@ -0,0 +1,4 @@
+__version__ = 'v0.0.0'
+# do not include any more lines of code. This is not a library; it is a CLI.
+# This version number lives here to be used internally and be available externally, but this module is not made to be
+# imported.
