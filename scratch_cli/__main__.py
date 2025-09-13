e52b95c22d486281e9c86c1dee47ef5c8ada5d4c
faretek <107722825+FAReTek1@users.noreply.github.com>
2025-09-13 12:48:29 +0100

    feat: login command, default to help

--- scratch_cli/__main__.py
+++ scratch_cli/__main__.py
@@ -1,10 +1,11 @@
 import argparse
+from typing import Literal
 
 from scratch_cli.util import ERROR_MSG
 from scratch_cli.__about__ import __version__
 
 class _Args(argparse.Namespace):
-    ...
+    command: Literal['login', None]
 
 def main():
     parser = argparse.ArgumentParser(
@@ -12,17 +13,26 @@ def main():
         description='Scratch command line interface',
         epilog=f"Scratch CLI {__version__}"
     )
+    commands = parser.add_subparsers(dest="command")
+
+    login = commands.add_parser("login", help="Login to Scratch")
+    login.add_argument("username")
+    login.add_argument("password")
 
     args = parser.parse_args(namespace=_Args())
 
     try:
-        cmd(args)
+        cmd(parser, args)
     except Exception as e:
         print(ERROR_MSG)
         raise e
 
-def cmd(args: _Args) -> None:
-    ...
+def cmd(parser: argparse.ArgumentParser, args: _Args) -> None:
+    match args.command:
+        case "login":
+            ...
+        case _:
+            parser.print_help()
 
 if __name__ == '__main__':
     main()
