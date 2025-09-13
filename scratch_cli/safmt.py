9f2f1b5a24b52beca58041f2b53ac22fc53eb622
faretek <107722825+FAReTek1@users.noreply.github.com>
2025-09-13 21:02:57 +0100

    feat: scli config

--- scratch_cli/safmt.py
+++ scratch_cli/safmt.py
@@ -1,12 +1,78 @@
 # format sa objects
-from typing import Optional, Iterable
+import json
+import warnings
+
+from typing import Optional, Iterable, TypedDict, NotRequired
 
 import scratchattach as sa
+from scratchattach import editor
+from scratchattach.utils import exceptions as sa_exceptions
 from scratch_cli import rfmt
+from scratch_cli.context import context
 
 from rich.color import Color
 
+import pydantic
+
 RESET = "\x1b[0m"
+
+def split_trailing_number(self: str) -> tuple[str, str]:
+    number = ""
+    while len(self) and self[-1].isnumeric():
+        number = self[-1] + number
+        self = self[:-1]
+
+    return self, number
+
+class SCLIConfig(TypedDict):
+    test: NotRequired[str]
+
+def scli_config(self: sa.User) -> SCLIConfig:
+    """
+    Get and return SCLI config data, if any. If none, return empty dict.
+    """
+    about_me, project_id = split_trailing_number(self.about_me)
+    if not project_id:
+        return {}
+
+    # project id may be valid, it may be not
+    try:
+        _project = context.session.connect_project(project_id)
+    except sa_exceptions.ProjectNotFound:
+        return {}
+
+    if _project.title != "_SCLI_CONFIG_":
+        return {}
+    if _project.author_name.lower() != self.username.lower():
+        return {}
+
+    _project_body = editor.Project.from_json(_project.raw_json())
+    _possible_comments = _project_body.stage.comments
+
+    if len(_possible_comments) != 1:
+        return {}
+
+    _comment = _possible_comments[0].text
+
+    if not _comment.endswith("_SCLI_CONFIG_"):
+        return {}
+
+    _comment = _comment[:-len("_SCLI_CONFIG_")]
+
+    try:
+        data = json.loads(_comment)
+
+    except json.decoder.JSONDecodeError:
+        warnings.warn(f"Could not decode SCLI comment: {_comment=}")
+        return {}
+
+    scli_validator = pydantic.TypeAdapter(SCLIConfig)
+
+    try:
+        return scli_validator.validate_python(data)
+    except pydantic.ValidationError:
+        warnings.warn(f"Invalid SCLI comment: {_comment=}")
+        return {}
 
 def collate(func, for_objs: Iterable) -> str:
     return '\n'.join(func(obj) for obj in for_objs)
@@ -37,6 +103,8 @@ def featured_project(self: Optional[dict[str, str | dict[str, str]]]):
 
 
 def user_profile(self: sa.User):
+    print(scli_config(self))
+
     ocular_data = self.ocular_status()
     ocular = 'No ocular status'
 
@@ -65,3 +133,6 @@ def user_profile(self: sa.User):
         message_count=self.message_count(),
         featured=featured_project(self.featured_data())
     )
+
+if __name__ == '__main__':
+    print(split_trailing_number("Browsing scratch in a terminal, far, far away...\n\n\n\n1216591875"))
