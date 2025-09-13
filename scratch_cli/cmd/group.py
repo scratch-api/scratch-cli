a9e35a1a6f6393149142c850ec69b8c66ff367d0
faretek <107722825+FAReTek1@users.noreply.github.com>
2025-09-13 17:29:14 +0100

    feat: switch group

--- scratch_cli/cmd/group.py
+++ scratch_cli/cmd/group.py
@@ -3,11 +3,14 @@ import argparse
 from scratch_cli.typed_cookies import cookies
 
 
+def print_all_groups():
+    for _group in cookies.groups.values():
+        print('-', _group["name"])
+
 def none():
     if cookies.current_group_name == '':
         print(f"Listing all groups, not in a group")
-        for _group in cookies.groups.values():
-            print('-', _group["name"])
+        print_all_groups()
         return
 
     _group = cookies.current_group
@@ -16,10 +19,21 @@ def none():
     for session in _group['sessions']:
         print('-', session['username'])
 
+def switch():
+    print(f"All groups::")
+    print_all_groups()
+
+    group_name = input("Enter group name: ")
+
+    assert group_name in cookies.groups, f"invalid name {group_name!r}"
+
+    cookies.current_group_name = group_name
 
 def group(parser: argparse.ArgumentParser, cmd):
     match cmd:
         case None:
             none()
+        case "switch":
+            switch()
         case _:
             parser.print_help()
